f2277c23c42fe15e41ae7822b406385de935f3e0
junayed-code <junayed.code@gmail.com>
2024-09-08 10:16:19 +0600

    chore(projects): supabase is setup in the-wild-oasis project
    Use supabase to manage the-wild-oasis backend database.

---
+++ projects/16-the-wild-oasis/src/services/supabase/index.ts
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './database.types';
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {});
+
+export default supabase;
