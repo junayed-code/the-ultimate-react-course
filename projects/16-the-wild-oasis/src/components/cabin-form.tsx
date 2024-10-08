d08c5175b2e7ec18110beb249e5d1a946bf83de3
junayed-code <junayed.code@gmail.com>
2024-10-08 09:17:11 +0600

    feat(projects): change the `CabinForm` fields layout to side by side
    The `CabinForm` fields layout was changed to side by side instead of
    top to bottom

--- projects/16-the-wild-oasis/src/components/cabin-form.tsx
+++ projects/16-the-wild-oasis/src/components/cabin-form.tsx
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 import { Formik, Form, FormikConfig, FormikValues } from 'formik';
 
+import Row from '@ui/row';
 import Input from '@ui/input';
 import { FormGroup, FormField, FormLabel, FormMessage } from '@ui/form';
 
 const StyledForm = styled(Form)`
   display: flex;
   flex-direction: column;
-  row-gap: 1.5rem;
+  row-gap: 1.25rem;
 `;
 
-const Textarea = styled(Input).attrs({ as: 'textarea' })``;
+const Textarea = styled(Input).attrs({ as: 'textarea' })`
+  textarea& {
+    height: 4.5rem;
+  }
+`;
 
 type CabinFormProps<Values> = Omit<
   FormikConfig<Values>,
@@ -24,29 +29,33 @@ function CabinForm<V extends FormikValues>(props: CabinFormProps<V>) {
     <Formik {...formik}>
       {formikProps => (
         <StyledForm className={className}>
-          <FormGroup name="name">
-            <FormLabel>Cabin name</FormLabel>
-            <FormField type="text" as={Input} />
-            <FormMessage />
-          </FormGroup>
+          <Row $align="start" $gap="1rem">
+            <FormGroup name="name">
+              <FormLabel>Cabin name</FormLabel>
+              <FormField type="text" as={Input} />
+              <FormMessage />
+            </FormGroup>
 
-          <FormGroup name="capacity">
-            <FormLabel>Maximum capacity</FormLabel>
-            <FormField type="number" as={Input} />
-            <FormMessage />
-          </FormGroup>
+            <FormGroup name="capacity">
+              <FormLabel>Maximum capacity</FormLabel>
+              <FormField type="number" as={Input} />
+              <FormMessage />
+            </FormGroup>
+          </Row>
 
-          <FormGroup name="price">
-            <FormLabel>Regular price</FormLabel>
-            <FormField type="number" as={Input} />
-            <FormMessage />
-          </FormGroup>
+          <Row $align="start" $gap="1rem">
+            <FormGroup name="price">
+              <FormLabel>Regular price</FormLabel>
+              <FormField type="number" as={Input} />
+              <FormMessage />
+            </FormGroup>
 
-          <FormGroup name="discount">
-            <FormLabel>Discount price</FormLabel>
-            <FormField type="number" as={Input} />
-            <FormMessage />
-          </FormGroup>
+            <FormGroup name="discount">
+              <FormLabel>Discount price</FormLabel>
+              <FormField type="number" as={Input} />
+              <FormMessage />
+            </FormGroup>
+          </Row>
 
           <FormGroup name="description">
             <FormLabel>Description</FormLabel>
