4a902b5990a8285cc03d1b8e08ec9737c5f473a2
junayed-code <junayed.code@gmail.com>
2024-11-01 12:01:55 +0600

    feat(projects): add a new variant called `link` to the `Button`
    Add a variant to the `Button` component that styles the `Button` to
    resemble a link. Variants are also created using the `createVariant`
    utils function.

--- projects/16-the-wild-oasis/src/components/ui/button.tsx
+++ projects/16-the-wild-oasis/src/components/ui/button.tsx
@@ -1,52 +1,59 @@
 import styled, { css } from 'styled-components';
+import { createVariant, type VariantProps } from '@utils/create-variant';
 
-const sizes = {
-  sm: css`
-    font-size: 0.75rem;
-    padding: 0.25rem 0.5rem;
-    letter-spacing: 0.02rem;
-    text-transform: uppercase;
-  `,
-  md: css`
-    font-size: 0.875rem;
-    padding: 0.5rem 1rem;
-  `,
-  lg: css`
-    font-size: 1rem;
-    padding: 0.625rem 1.75rem;
-  `,
-  icon: css`
-    width: 2.375rem;
-    height: 2.375rem;
-    font-size: 1rem;
-  `,
-};
+const buttonVariant = createVariant({
+  $variant: {
+    primary: css`
+      --color-hover: var(--color-brand-600);
+      color: var(--color-brand-50);
+      background-color: var(--color-brand-500);
+    `,
+    secondary: css`
+      --color-hover: var(--color-grey-100);
+      color: var(--color-grey-600);
+      background: var(--color-grey);
+      border-color: var(--color-grey-300);
+    `,
+    danger: css`
+      --color-hover: var(--color-red-800);
+      color: var(--color-red-100);
+      background-color: var(--color-red-700);
+    `,
+    link: css`
+      color: var(--color-brand-500);
+      background-color: initial;
+      &:enabled:hover {
+        color: var(--color-brand-600);
+      }
+    `,
+  },
+  $size: {
+    initial: css`
+      font-size: 0.875rem;
+    `,
+    sm: css`
+      font-size: 0.75rem;
+      padding: 0.25rem 0.5rem;
+      letter-spacing: 0.02rem;
+      text-transform: uppercase;
+    `,
+    md: css`
+      font-size: 0.875rem;
+      padding: 0.5rem 1rem;
+    `,
+    lg: css`
+      font-size: 1rem;
+      padding: 0.625rem 1.75rem;
+    `,
+    icon: css`
+      width: 2.375rem;
+      height: 2.375rem;
+      font-size: 1rem;
+    `,
+  },
+});
 
-const variants = {
-  primary: css`
-    --color-hover: var(--color-brand-600);
-    color: var(--color-brand-50);
-    background-color: var(--color-brand-500);
-  `,
-  secondary: css`
-    --color-hover: var(--color-grey-100);
-    color: var(--color-grey-600);
-    background: var(--color-grey);
-    border-color: var(--color-grey-300);
-  `,
-  danger: css`
-    --color-hover: var(--color-red-800);
-    color: var(--color-red-100);
-    background-color: var(--color-red-700);
-  `,
-};
-
-type ButtonProps = {
-  $size?: 'sm' | 'md' | 'lg' | 'icon';
-  $variant?: 'primary' | 'secondary' | 'danger';
-};
-
-const Button = styled.button<ButtonProps>`
+const Button = styled.button<VariantProps<typeof buttonVariant>>`
   display: inline-flex;
   align-items: center;
   justify-content: center;
@@ -65,8 +72,9 @@ const Button = styled.button<ButtonProps>`
     transform: scale(0.95);
   }
 
-  ${({ $size = 'md' }) => sizes[$size]}
-  ${({ $variant = 'primary' }) => variants[$variant]}
+  ${({ $size, $variant }) => buttonVariant({ $variant, $size })}
 `;
+
+Button.defaultProps = { $variant: 'primary', $size: 'md' };
 
 export default Button;
