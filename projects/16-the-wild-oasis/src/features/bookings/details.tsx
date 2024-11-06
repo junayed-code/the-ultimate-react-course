cadc5390fc261240fbe65e46a9cce79512da561b
junayed-code <junayed.code@gmail.com>
2024-11-06 10:29:59 +0600

    feat(projects): add three actions button on the booking details page
    - one for deleting a booking
    - one for navigating backward
    - one for navigating to the checkin page

--- projects/16-the-wild-oasis/src/features/bookings/details.tsx
+++ projects/16-the-wild-oasis/src/features/bookings/details.tsx
@@ -1,24 +1,48 @@
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import styled from 'styled-components';
+import { Link, useParams } from 'react-router-dom';
 
 import Row from '@ui/row';
 import Tag from '@ui/tag';
 import Button from '@ui/button';
 import Spinner from '@ui/spinner';
+import ConfirmDeleteModal from '@ui/confirm-delete-modal';
 import BookingDataBox from '@features/bookings/data-box';
+import { type ModalContextValue } from '@ui/modal';
 
-import { useBooking } from '@hooks/bookings';
+import { useBooking, useDeleteBooking } from '@hooks/bookings';
 import { useMoveBack } from '@hooks/use-move-back';
 import { pickTagVariant } from '@utils/pick-tag-variant';
+
+const ModalOpenButton = styled(Button).attrs({
+  $variant: 'danger',
+  children: 'Delete Booking',
+})``;
+
+type ButtonProps = React.ComponentProps<typeof Button>;
+const LinkButton = styled(Link).attrs<ButtonProps>({
+  $size: 'md',
+  $variant: 'primary',
+})`
+  ${Button.componentStyle.rules}
+`;
 
 function BookingDetails() {
   const { id } = useParams();
   const moveBack = useMoveBack();
   const { booking, isLoading } = useBooking(id!);
+  const { bookingDeleteTrigger, isBookingDeleting } = useDeleteBooking(
+    Number(id),
+  );
 
   if (isLoading) return <Spinner />;
   if (!booking) return null;
 
   const { status } = booking;
+
+  function handleDeleteBooking(arg: ModalContextValue) {
+    bookingDeleteTrigger().finally(arg.handleClose).then(moveBack);
+  }
 
   return (
     <>
@@ -35,6 +59,21 @@ function BookingDetails() {
       </Row>
 
       <BookingDataBox booking={booking} />
+
+      <Row $justify="end" $gap="0.75rem">
+        {status === 'unconfirmed' && (
+          <LinkButton to={`/checkin/${id}`}>Check in</LinkButton>
+        )}
+        <ConfirmDeleteModal
+          resourceName={`booking#${id}`}
+          OpenButtonCom={ModalOpenButton}
+          isDeleting={isBookingDeleting}
+          onConfirm={handleDeleteBooking}
+        />
+        <Button $variant="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </Row>
     </>
   );
 }
