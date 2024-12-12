ae40d4db4486ff5b2f0ef7eacd034984cb5d086e
junayed-code <junayed.code@gmail.com>
2024-12-12 10:07:55 +0600

    feat(projects): declare typescript overloads for `useLocalStorage` hook
    Set type of the initialState parameter of the `useLocalStorage` hook to a
    function or a value by using the typescript function overloads. This
    allows us to take a function or a value as the initalState.

--- projects/16-the-wild-oasis/src/hooks/use-local-storage.ts
+++ projects/16-the-wild-oasis/src/hooks/use-local-storage.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function isValidJSON(value: string) {
   try {
@@ -8,11 +8,26 @@ function isValidJSON(value: string) {
   }
 }
 
-export function useLocalStorage<S>(initialState: S, key: string) {
+export function useLocalStorage<S>(
+  initialState: S,
+  key: string,
+): [S, React.Dispatch<React.SetStateAction<S>>];
+export function useLocalStorage<S>(
+  initialState: () => S,
+  key: string,
+): [S, React.Dispatch<React.SetStateAction<S>>];
+export function useLocalStorage<S>(initialState: unknown, key: string) {
   const [value, setValue] = useState<S>(function () {
     const storedValue = localStorage.getItem(key);
+    // If initialState is a function, then call the function to get
+    // the state value
+    const stateValue =
+      typeof initialState === 'function' ? initialState() : initialState;
+    // If the stored value is found, then use it otherwise use the
+    // initialState value. Convert the storedValue to an object if it's a
+    // valid JSON otherwise, use it as it is.
     return !storedValue
-      ? initialState
+      ? stateValue
       : isValidJSON(storedValue)
         ? JSON.parse(storedValue)
         : storedValue;
@@ -24,5 +39,5 @@ export function useLocalStorage<S>(initialState: S, key: string) {
     localStorage.setItem(key, _value);
   }, [value, key]);
 
-  return [value, setValue] as const;
+  return [value, setValue];
 }
