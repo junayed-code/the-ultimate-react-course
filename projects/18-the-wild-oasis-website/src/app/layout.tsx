59095febdcdd6dc1cbf6d270f2e8c86fb281a546
junayed-code <junayed.code@gmail.com>
2024-12-24 18:16:44 +0600

    chore(projects): configure the fonts, tailwindcss, and metadata
    Setup the tailwindcss with app colors and fonts variables and import
    `josefin sans` font from google to use it as default app font. Set the
    base title and description metadata in the app root layout file. Add
    some base classes in the `globals.css` file.

--- projects/18-the-wild-oasis-website/src/app/layout.tsx
+++ projects/18-the-wild-oasis-website/src/app/layout.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import type { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -8,25 +8,30 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const josefinSans = Josefin_Sans({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-josefin-sans",
 });
 
 export const metadata: Metadata = {
-  title: "The Wild Oasis Website",
-  description: "The wild oasis website",
+  title: {
+    default: "Welcome / The Wild Oasis",
+    template: "%s / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabins hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${josefinSans.variable} font-sans antialiased bg-primary-950 text-primary-100`}
       >
         <main>{children}</main>
       </body>
