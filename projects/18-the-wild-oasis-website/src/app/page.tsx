01dd7c42d816044938c4a330ddfc64b90f04e0aa
junayed-code <junayed.code@gmail.com>
2024-12-26 07:40:55 +0600

    feat(projects): implement the application homepage
    Accomplish the homepage with a nice background image, a big header, and
    a callout button.

--- projects/18-the-wild-oasis-website/src/app/page.tsx
+++ projects/18-the-wild-oasis-website/src/app/page.tsx
@@ -1,12 +1,32 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import Container from "@components/container";
+import bgImage from "@images/bg.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-5xl font-bold">
-        This is the root page of the application
-      </h1>
-      <Link href="/cabins">Cabin Page</Link>
-    </div>
+    <Container className="relative">
+      {/* Background image of the page */}
+      <Image
+        src={bgImage}
+        placeholder="blur"
+        className="object-cover object-top fixed inset-0 w-full h-full"
+        alt="Mountains and forests with two cabins"
+      />
+
+      {/* The page content */}
+      <div className="w-full min-h-fit absolute top-[78%] sm:top-[58%] -translate-y-1/2 z-10 text-center">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-primary-50 mb-4 md:mb-6 tracking-tight">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="inline-block bg-accent-500 px-7 md:px-9 py-3.5 md:py-5 text-primary-800 text-sm md:text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </Container>
   );
 }
