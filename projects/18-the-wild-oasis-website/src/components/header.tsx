3379ba1919a59277c1adec7372fd4d355a4db500
junayed-code <junayed.code@gmail.com>
2024-12-29 07:33:49 +0600

    feat(projects/18): add html attributes to the header component
    HTML attributes are added to the header component to customize it as
    needed.

--- projects/18-the-wild-oasis-website/src/components/header.tsx
+++ projects/18-the-wild-oasis-website/src/components/header.tsx
@@ -1,9 +1,20 @@
+import type { HTMLAttributes } from "react";
+
 import Logo from "@ui/logo";
 import Navigation from "@components/navigation";
+import { cn } from "@utils/cn";
 
-function Header() {
+type HeaderProps = HTMLAttributes<HTMLElement>;
+
+function Header({ className, ...props }: HeaderProps) {
   return (
-    <header className="border-b border-primary-900 px-5 md:px-8 py-4">
+    <header
+      {...props}
+      className={cn(
+        "border-b border-primary-900 px-5 md:px-8 py-4 h-[4.25rem]",
+        className,
+      )}
+    >
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <Logo />
         <Navigation />
