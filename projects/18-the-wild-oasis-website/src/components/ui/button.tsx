edf811bd3325c38aae989422a302b7d25675b683
junayed-code <junayed.code@gmail.com>
2025-01-11 11:42:23 +0600

    feat(projects/18): add `Button` component
    This button component is designed with default styles for this
    application.

---
+++ projects/18-the-wild-oasis-website/src/components/ui/button.tsx
@@ -0,0 +1,26 @@
+import { cloneElement, isValidElement, type ButtonHTMLAttributes } from "react";
+import { cn } from "@utils/cn";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  asChild?: boolean;
+};
+
+function Button({ asChild, children, ...props }: ButtonProps) {
+  const className = cn(
+    `bg-accent-500 text-primary-800 hover:bg-accent-600 inline-flex items-center justify-center gap-2 px-6 py-2.5 text-sm whitespace-nowrap transition-all md:text-lg disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300`,
+    props.className,
+  );
+
+  const element =
+    asChild && isValidElement<ButtonProps>(children) ? (
+      cloneElement(children, { ...props, className })
+    ) : (
+      <button className={className} {...props}>
+        {children}
+      </button>
+    );
+
+  return element;
+}
+
+export default Button;
